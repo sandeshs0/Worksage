1eb87dddb3b09b6aefd6e2f2abf7a7c1fe92d2ef
sandeshs0 <hellosandesh0@gmail.com>
2025-07-24 23:11:26 +0545

    HTTPS Server with SSL Certificate

--- cubicle-frontend/vite.config.js
+++ cubicle-frontend/vite.config.js
@@ -1,4 +1,6 @@
 import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -6,6 +8,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    allowedHosts: ["cubicle-crm-frontend.onrender.com"],
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "cert", "key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "cert", "cert.pem")),
+    },
   },
 });
